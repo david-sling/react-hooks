0702b4d4b97ac9ee322a13bbafa8a229156d57a8
David S D <sddavid.johan@gmail.com>
2022-07-28 19:59:49 +0530

    fix : example bug

--- example/src/components/LocalStorage.tsx
+++ example/src/components/LocalStorage.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { useLocalStorage } from '@david-sling/react-hooks'
 
 export default function LocalStorage() {
-  const [text, setText] = useLocalStorage<string>('text')
+  const [text, textProps] = useLocalStorage<string>('text')
   return (
     <div>
       <h2>useLocalStorage</h2>
       <input
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => textProps.set(e.target.value)}
         type='text'
       />
     </div>
